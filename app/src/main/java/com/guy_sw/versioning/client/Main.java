c389fb9ee453cd529b720f9a338a857ad48141d7
Guy Yafe <gyafe@tripactions.com>
2023-06-29 12:53:56 +0300

    Simplest Main Method

--- app/src/main/java/com/guy_sw/versioning/client/Main.java
+++ app/src/main/java/com/guy_sw/versioning/client/Main.java
@@ -5,7 +5,7 @@ package com.guy_sw.versioning.client;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome To Versioning Project Client!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Welcome To Versioning Project Weather Client!");
+  }
 }
