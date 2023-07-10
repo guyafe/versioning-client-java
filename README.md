eb61cce2ba710ebe4457aa8c581736273305a358
Guy Yafe <gyafe@tripactions.com>
2023-07-10 10:28:39 +0300

    Create Client

--- README.md
+++ README.md
@@ -2,15 +2,38 @@
 
 The versioning project is a demo to a process of correctly versioning a project, and based on the working branch automatically setting the dependencies.
 
-This project used Java 17 and Gradle 8.1.1.
+This project uses Java 17 and Gradle 8.1.1.
 
 This repository demonstrates the Java client of a weather service.<br>
 At the moment we don't do any versioning for the code.
 
 ## Usage
+
 ### Clone
 First clone the project from GitHub: https://github.com/guyafe/versioning-client-java.git
-### Build
-Build is using the command `gradle build`. It will create a jar file named `app.jar` in the `build/libs` directory.
+
 ### Run
 Run the client using `gradle run`. It will run the client with the default parameters.
+
+### Command Line Usage
+Usage:
+
+`health - Shows server health status`<br>
+`weather <city> - return the weather for the next day in the given city`<br>
+`exit - exits application`
+
+## Gradle Usage
+
+### Build
+
+Using the command `gradle build` will create an executable jar artifact in the `build/libs` directory.<br>
+Depending on the current branch:
+* If the current branch is main, the artifact will be named _\<major\>_._\<minor\>_._\<patch\>_
+* If the current branch is not main, the artifact will be named _\<major\>_._\<minor\>_._\<branch\>_-_SNAPSHOT_
+
+### Publish
+In order to publish this artifact, and have it available for other repositories, use the command `gradle publishToMavenLocal`.<br>
+* If the current branch is main:
+    1. The task will publish the artifact into local repository (usually in `~/.m2/repository`).
+    2. Next, the task will check if the branch is clean (no uncommitted changes).. If so, it will bump the _minor_ version and commit the change with **[NO CI]** message.
+* If the current branch is not main, the task will publish the artifact into local repository.
--- app/src/main/java/com/guy_sw/versioning/client/Main.java
+++ app/src/main/java/com/guy_sw/versioning/client/Main.java
@@ -1,8 +1,70 @@
 package com.guy_sw.versioning.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.Gson;
+import com.guy_sw.versioning.common_models.WeatherStatus;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, InterruptedException {
     System.out.println("Welcome To Versioning Project Weather Client!");
+    printUsage();
+    boolean shouldRun = true;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    while (shouldRun) {
+      String userCommand = br.readLine();
+      String[] commandTokens = userCommand.split(" +");
+      switch (commandTokens[0]) {
+        case "exit" -> shouldRun = false;
+        case "health" -> sendHealthRequest();
+        case "weather" -> sendCityWeatherRequest(commandTokens);
+        default -> printUsage();
+      }
+
+    }
+  }
+
+  private static void sendCityWeatherRequest(String[] tokens) throws IOException, InterruptedException {
+    if(tokens.length != 2) {
+      printUsage();
+    } else {
+      String city = tokens[1];
+      HttpRequest request = HttpRequest.newBuilder()
+                                       .uri(URI.create("http://localhost:61285/weather/" + city))
+                                       .GET()
+                                       .build();
+      HttpClient client = HttpClient.newHttpClient();
+      String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+      WeatherStatus weatherStatus = new Gson().fromJson(response, WeatherStatus.class);
+      System.out.println("Weather status for " + city + ": ");
+      System.out.println("\tTemperature: " + weatherStatus.getTemperatureCelsius() + " Celsius");
+      System.out.println("\tWind direction: " + weatherStatus.getWindDirection());
+    }
+  }
+
+  private static void sendHealthRequest() throws IOException, InterruptedException {
+    HttpRequest request = HttpRequest.newBuilder()
+                                     .uri(URI.create("http://localhost:61285/weather"))
+                                     .GET()
+                                     .build();
+    HttpClient client = HttpClient.newHttpClient();
+    String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    System.out.println("Server health status: " + response);
+  }
+
+  private static void printUsage() {
+    System.out.println("Usage:");
+    System.out.println("\thealth - Shows server health status");
+    System.out.println("\tweather <city> - return the weather for the next day in the given city");
+    System.out.println("\texit - exits application");
+    System.out.println("Please enter your command:");
+    System.out.println();
   }
 }
